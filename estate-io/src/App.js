092d02f4a8869561662e10fe53f4074d98925f43
Andrei-Cristian Sandor <andreicsandor@gmail.com>
2023-03-22 17:15:29 +0200

    Update App.js routes

--- estate-io/src/App.js
+++ estate-io/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import About from './pages/About';
+import Commercial from './pages/Commercial';
+import Contact from './pages/Contact';
+import Header from './Header';
+import Home from './pages/Home';
+import News from './pages/News';
+import Residential from './pages/Residential';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/residential" element={<Residential />} />
+          <Route path="/commercial" element={<Commercial />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </>
+  )
 }
 
 export default App;
