f06765d44b75b544715c4422716e6b2e377b856c
Andrei-Cristian Sandor <andreicsandor@gmail.com>
2023-04-04 19:22:20 +0300

    Remove react-sticky

--- estate-io/src/App.js
+++ estate-io/src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import { StickyContainer, Sticky } from 'react-sticky';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import About from './pages/About';
@@ -14,19 +15,19 @@ import ResidentialRent from './pages/ResidentialRent';
 function App() {
   return (
     <>
-      <Header />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/news" element={<News />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/residential-buy" element={<ResidentialBuy />} />
-          <Route path="/residential-rent" element={<ResidentialRent />} />
-          <Route path="/commercial-buy" element={<CommercialBuy />} />
-          <Route path="/commercial-rent" element={<CommercialRent />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
+    <Header  />
+    <div className="container-custom">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/residential-buy" element={<ResidentialBuy />} />
+        <Route path="/residential-rent" element={<ResidentialRent />} />
+        <Route path="/commercial-buy" element={<CommercialBuy />} />
+        <Route path="/commercial-rent" element={<CommercialRent />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
     </>
   )
 }
