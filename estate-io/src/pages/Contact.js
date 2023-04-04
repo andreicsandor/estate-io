d323794ee839147698193ac89714007c1f0aa53f
Andrei-Cristian Sandor <andreicsandor@gmail.com>
2023-04-04 19:22:56 +0300

    Add Contact page

--- estate-io/src/pages/Contact.js
+++ estate-io/src/pages/Contact.js
@@ -1,5 +1,38 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Form, FormGroup, Label, Input, Button } from 'reactstrap';
 
+function Contact() {
+  return (
+    <>
+    <div className='container-about'>
+    <div style={{margin: '100px'}}></div>
+    <Container className='my-5'>
+      <Row>
+        <Col>
+          <h1 className='display-5 text-center mb-5'>Contact Us</h1>
+        </Col>
+      </Row>
+      <Row className='justify-content-center mb-4'>
+        <Col md={{size: 8}}>
+          <p className='lead text-center'>We're always here to help. If you have any questions or comments, please fill out the form below and we'll get back to you as soon as possible.</p>
+        </Col>
+      </Row>
+      <Row className='justify-content-center'>
+        <Col md={{size: 6}}>
+          <Form>
+            <Input placeholder="Name" bsSize="lg" className='mb-3'/>
+            <Input placeholder="E-mail" bsSize="default" className='mb-3'/>
+            <Input placeholder="Phone" bsSize="default" className='mb-3'/>
+            <Input type='textarea' name='message' id='message' rows={6} placeholder='Enter your message' className='form-control mb-5' />
+            <Button color='dark' block>Submit</Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+    </div>
+    </>
+  );
+}
 
-export default function Contact() {
-    return <h1>Contact</h1>
-  }
+export default Contact;
