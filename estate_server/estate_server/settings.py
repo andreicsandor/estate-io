1c7df8f95a9a6129fff9533a234adfae1788f198
Andrei-Cristian Sandor <andreicsandor@gmail.com>
2023-04-12 11:27:39 +0300

    Update project settings

--- estate_server/estate_server/settings.py
+++ estate_server/estate_server/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'estate_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Estate',
+        'USER': 'estate_admin',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '8889',
     }
 }
 
