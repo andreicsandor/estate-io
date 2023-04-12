366b708f48f19c09a7f8f83cec394ef5be6e3695
Andrei-Cristian Sandor <andreicsandor@gmail.com>
2023-04-12 11:27:28 +0300

    Create backend app

--- estate_server/app_backend/apps.py
+++ estate_server/app_backend/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BackendConfig(AppConfig):
+class AppBackendConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'backend'
+    name = 'app_backend'
